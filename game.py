aff5d4faf306b07898d33db66258e6e3844d3875
codywillard <gadawg099@gmail.com>
2023-04-08 23:02:40 -0400

    Finished all functionality and GUI elements.

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ class Game:
         self.player_one = 'X'
         self.player_two = 'O'
         self.player_one_turn = True
+        self.winning_combination = None
 
     def create_board(self):
         board = [['', '', ''],
@@ -20,28 +21,34 @@ class Game:
     # Checks the board for all the combinations the player could win with
     def check_for_win(self, board, player):
         # Checks rows for three across
-        for row in board:
-            if row.count(player) == 3:
+        for row in range(len(board)):
+            if board[row].count(player) == 3:
+                print('won')
+                self.winning_combination = (row, 0, row, 1, row, 2)
                 return True
-
         # Checks through columns for three down
         for i in range(0, len(board)):
             if board[0][i] == player and board[1][i] == player and board[2][i] == player:
+                print('won')
+                self.winning_combination = (0, i, 1, i, 2, i)
                 return True
-
         # checks for cross pattern from top left to bottom right
         if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+            print('won')
+            self.winning_combination = (0, 0, 1, 1, 2, 2)
             return True
-
         # checks for cross pattern from top right to bottom left
-        return board[0][2] == player and board[1][1] == player and board[2][0] == player
+        elif board[0][2] == player and board[1][1] == player and board[2][0] == player:
+            print('won')
+            self.winning_combination = (0, 2, 1, 1, 2, 0)
+            return True
 
     # checks to see if the board is full
     def check_if_full(self, board):
         for i in board:
             if '' in i:
-                return True
-        return False
+                return False
+        return True
 
     def turn(self):
         if self.player_one_turn:
@@ -50,3 +57,7 @@ class Game:
 
         self.player_one_turn = True
         return self.player_two
+
+    def winning_combo(self):
+        return self.winning_combination
+
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from gui import Gui
-import gui
-import customtkinter as ctk
 import game
 
 
@@ -19,7 +17,7 @@ class App:
             self.game.add_to_board(self.board, player, row, col)
             self.update_button()
             if self.game.check_for_win(self.board, player):
-                self.win(player)
+                self.win()
             if self.game.check_if_full(self.board):
                 self.draw()
 
@@ -28,23 +26,37 @@ class App:
             for col in range(3):
                 self.gui.buttons_text[row][col].set(self.board[row][col])
 
-    def win(self, row, col):
-        pass
+    def win(self):
+        combo = self.game.winning_combo()
+        r1, c1, r2, c2, r3, c3 = combo
+        rows = [r1, r2, r3]
+        cols = [c1, c2, c3]
+        for row in range(3):
+            for col in range(3):
+                self.gui.buttons[row][col].configure(fg_color=('#13293e', '#13293e'), state='disabled')
+        for i in range(3):
+            self.gui.buttons[rows[i]][cols[i]].configure(fg_color=('#3a7ebf', '#1f538d'))
 
     def draw(self):
-        pass
+        for row in range(3):
+            for col in range(3):
+                self.gui.buttons[row][col].configure(state='disabled')
 
     def new_game(self):
         for row in range(3):
             for col in range(3):
                 self.board[row][col] = ''
+        self.configure_buttons()
         self.update_button()
         self.game.player_one_turn = True
+        self.game.winning_combination = None
 
     def configure_buttons(self):
         for row in range(3):
             for col in range(3):
-                self.gui.buttons[row][col].configure(command=lambda i=row, x=col: self.button_press(i, x))
+                self.gui.buttons[row][col].configure(fg_color=('#3a7ebf', '#1f538d'),
+                                                     state='normal',
+                                                     command=lambda i=row, x=col: self.button_press(i, x))
         self.gui.new_game_button.configure(command=lambda: self.new_game())
 
 
